f340c58544abe60f7e5f7876d9fc2c9f225f105d
Baddam-Charitha <66307794+Baddam-Charitha@users.noreply.github.com>
2022-12-29 16:07:01 +0530

    Add files via upload

---
+++ Random_Password.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Random_Password {
+    public static void main(String args[]) {
+
+        //Scanner sc=new Scanner(System.in);
+        String pass=genPass(8,16);
+        System.out.println("Random Password : "+pass);
+     
+    }
+
+    static String genPass(int min, int max){
+
+        Random random=new Random();
+        String upper="QWERTYUIOPASDFGHJKLZXCVBNM";
+        String lower="qwertyuiopasdfghjklzxcvbnm";
+        String number="1234567890";
+        String special="!@#$%^&*(){}?";
+        String all=upper+lower+number+special;
+        List<Character> letterslist=new ArrayList<Character>();
+        for(char c: all.toCharArray()){
+            letterslist.add(c);
+
+        }
+        Collections.shuffle(letterslist);
+        String pass="";
+        for(int i=random.nextInt(max-min)+min;i>0;--i){
+            pass+=letterslist.get(random.nextInt(letterslist.size()));
+
+        }
+        return pass;
+    }
+}
